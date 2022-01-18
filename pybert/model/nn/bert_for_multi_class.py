e4ab9cb9610bfaa8420a40b63f9f4050d58636ee
tianchang93824 <tianchang824@outlook.com>
2022-01-18 18:17:28 +0800

    单独初始化

--- pybert/model/nn/bert_for_multi_class.py
+++ pybert/model/nn/bert_for_multi_class.py
@@ -8,7 +8,8 @@ class BertForMultiClass(BertPreTrainedModel):
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
-        self.apply(self.init_weights)
+        #self.apply(self.init_weights)
+        self.init_weights()
 
     def forward(self, input_ids, token_type_ids=None, attention_mask=None, head_mask=None):
         outputs = self.bert(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask,
