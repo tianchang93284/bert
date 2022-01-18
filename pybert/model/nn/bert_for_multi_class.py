8c73f73e148869afd887ad745ab57759213428dc
tianchang93824 <tianchang824@outlook.com>
2022-01-18 17:26:26 +0800

    修改apply错误

--- pybert/model/nn/bert_for_multi_class.py
+++ pybert/model/nn/bert_for_multi_class.py
@@ -9,7 +9,6 @@ class BertForMultiClass(BertPreTrainedModel):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
         self.apply(self.init_weights)
-        #self.bertapply(self.init_weights)
 
     def forward(self, input_ids, token_type_ids=None, attention_mask=None, head_mask=None):
         outputs = self.bert(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask,
